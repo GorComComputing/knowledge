5536004c2869baaab7b90abcc341461fee444b38
Evgeny Goryachev <108394522+GorComComputing@users.noreply.github.com>
2023-07-08 15:13:38 +0400

    Update hello.pas

--- pascal/hello.pas
+++ pascal/hello.pas
@@ -1,4 +1,17 @@
 program hello;
+
+var
+	x: integer = 500;
+ 	y: integer;
+  	z: longint;
+  	flag: boolean;
+   
 begin
-	writln('Hello world!')
+	writln('Hello world!');
+ 	write('hello');
+  	writeln('173 * 81 =', 173 * 81);	// div, mod
+
+   	x := 79;
+
+     	read(x);
 end.
